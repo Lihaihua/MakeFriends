2b34c82021bd7f31e27b89986eed5a6c77707cda
Lihaihua <haih1983@hotmail.com>
2016-04-25 23:31:34 +0800

    登录界面
    登录界面

--- app/src/main/java/com/mark/makefriends/login/LoginActivity.java
+++ app/src/main/java/com/mark/makefriends/login/LoginActivity.java
@@ -1,21 +1,81 @@
 package com.mark.makefriends.login;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mark.makefriends.R;
-import com.mark.makefriends.bean.Person;
 
+import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.listener.SaveListener;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener{
     private static final String TAG = "MainActivity";
+    private EditText userName;
+    private EditText password;
+    private Button loginBtn;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        userName = (EditText)findViewById(R.id.user_name);
+        password = (EditText)findViewById(R.id.password);
+        loginBtn = (Button)findViewById(R.id.login);
+        registerBtn = (Button)findViewById(R.id.register);
+
+        loginBtn.setOnClickListener(this);
+        registerBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                if(userName.getText().toString().equals("") || password.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "账号密码不能为空!", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                BmobUser bu2 = new BmobUser();
+                bu2.setUsername(userName.getText().toString());
+                bu2.setPassword(password.getText().toString());
+                bu2.login(this, new SaveListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getApplicationContext(), "登录成功!", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onFailure(int i, String s) {
+                        Toast.makeText(getApplicationContext(), "登录失败!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
+
+            case R.id.register:
+                BmobUser bu = new BmobUser();
+                bu.setUsername(userName.getText().toString());
+                bu.setPassword(password.getText().toString());
+                bu.signUp(this, new SaveListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getApplicationContext(), "注册成功!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(int i, String s) {
+                        Toast.makeText(getApplicationContext(), "注册失败!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
+            default:
+                break;
+        }
     }
 }
